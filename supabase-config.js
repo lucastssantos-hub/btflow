510a7853596bde214d96d1f729ada7cb80538219
lucastssantos-hub <lucas.tssantos@gmail.com>
2026-05-25 13:46:47 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-window.BEACHFLOW_SUPABASE = {
-  url: "https://btjsweysefmbceqqlyxx.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0anN3ZXlzZWZtYmNlcXFseXh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNzk2ODQsImV4cCI6MjA5NDg1NTY4NH0.9_JLTjzMWj5apV75H_Y-qtEE24pBrz7bdONP8EHvNdQ"
-};
-
-window.BEACHFLOW_AI = {
-  useEdgeFunction: true,
-  endpoint: "https://btjsweysefmbceqqlyxx.supabase.co/functions/v1/gerar-treino-bt"
-};
