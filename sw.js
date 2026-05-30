e82978f71d7bc6fcc5d20147ed02ba2142b24058
lucastssantos-hub <lucas.tssantos@gmail.com>
2026-05-30 14:01:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'beachflow-v18-compact-cycle';
+const CACHE = 'beachflow-v19-motor-diagnostico';
 const asset = path => new URL(path, self.registration.scope).toString();
 const ASSETS = [
   asset('./'),
